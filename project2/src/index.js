40bfea31e023b1bbd09eabf06c26aa1ecfd28de6
Alec Buchanan <alecbuchanan@github.com>
2024-01-16 13:52:04 -0500

    Update index.js

--- project2/src/index.js
+++ project2/src/index.js
@@ -1,3 +1,3 @@
 // This is the main entry point for Project 2
 console.log('Hello, world from Project 2!');
-console.log("Welcome to my first Node.js project!");
+console.log("Welcome to my first Node.js project! Enjoy your stay.");
