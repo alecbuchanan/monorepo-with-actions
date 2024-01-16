90738f28e21d92c68d5ee144112bc219a880c587
Alec Buchanan <alecbuchanan@github.com>
2024-01-16 17:02:52 -0500

    Updated project3 (#4)
    Reorganizing the GitHub Actions workflows

--- project3/src/index.js
+++ project3/src/index.js
@@ -1,3 +1,3 @@
 // This is the main entry point for Project 3
 console.log('Hello, this is Project 3!');
-console.log("Welcome to my first Node.js project!");
+console.log("Welcome to my first Node.js project! Enjoy your stay.");
